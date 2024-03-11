106ca90ae7591e4d1115186f3cc387d8cf6e91dc
Yuli Wang <yuliwang1007@gmail.com>
2024-03-11 00:31:02 -0600

    add comments

--- ComplexType/enum.ts
+++ ComplexType/enum.ts
@@ -11,6 +11,7 @@ let ordersArray:[Pet, number][] = [
   [Pet.Chinchilla, 1],
   [Pet.Hamster, 3]
 ]
+//ordersArray is an Array of tuple, with type Pet and number
 
 // ordersArray.push([Pet.Rabbit, 2]);
 //Property 'Rabbit' does not exist on type 'typeof Pet'.
