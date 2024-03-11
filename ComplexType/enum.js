47f625c47e9ee90d6c25f85df08009e30d5f9bcc
Yuli Wang <yuliwang1007@gmail.com>
2024-03-11 00:28:48 -0600

    create enum Pet

---
+++ ComplexType/enum.js
@@ -0,0 +1,14 @@
+"use strict";
+var Pet;
+(function (Pet) {
+    Pet[Pet["Hamster"] = 0] = "Hamster";
+    Pet[Pet["Rat"] = 1] = "Rat";
+    Pet[Pet["Chinchilla"] = 2] = "Chinchilla";
+    Pet[Pet["Tarantula"] = 3] = "Tarantula";
+})(Pet || (Pet = {}));
+let petOnSale = Pet.Hamster;
+let ordersArray = [
+    [Pet.Rat, 2],
+    [Pet.Chinchilla, 1],
+    [Pet.Hamster, 3]
+];
---
+++ ComplexType/enum.ts
@@ -0,0 +1,14 @@
+enum Pet {
+  Hamster,
+  Rat,
+  Chinchilla,
+  Tarantula
+}
+
+let petOnSale :Pet = Pet.Hamster
+let ordersArray:[Pet, number][] = [
+  [Pet.Rat, 2],
+  [Pet.Chinchilla, 1],
+  [Pet.Hamster, 3]
+]
+
