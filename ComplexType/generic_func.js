02e0506d996ef0f14e8b3e3f9f7dd6cacb02163e
Yuli Wang <yuliwang1007@gmail.com>
2024-03-12 17:13:08 -0600

    create generic_func

---
+++ ComplexType/generic_func.js
@@ -0,0 +1,13 @@
+"use strict";
+function getFilledArray(value, n) {
+    return Array(n).fill(value);
+}
+let stringArray;
+let numberArray;
+let personArray;
+let coordinateArray;
+stringArray = getFilledArray('hi', 6);
+numberArray = getFilledArray(9, 6);
+personArray = getFilledArray({ name: 'J. Dean', age: 24 }, 6);
+coordinateArray = getFilledArray([3, 4], 6);
+console.log(coordinateArray);
