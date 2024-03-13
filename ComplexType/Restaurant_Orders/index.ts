882c626f2ac971b88fee69ef456affa29a58ff98
Yuli Wang <yuliwang1007@gmail.com>
2024-03-12 21:05:35 -0600

    getOrders and printOrders

--- ComplexType/Restaurant_Orders/index.ts
+++ ComplexType/Restaurant_Orders/index.ts
@@ -1,3 +1,44 @@
 import { PriceBracket, orders, Order } from "./orders";
 import { restaurants, Restuanrant } from "./restaurants";
 
+const getMaxPrice = (priceRange: PriceBracket):number => {
+  switch(priceRange) {
+    case PriceBracket.Low:
+      return 10.0;
+    case PriceBracket.Medium:
+      return 20.0;
+    case PriceBracket.High:
+      return 30.0;
+    default:
+      return 0
+  }
+}
+
+const getOrders = (price: PriceBracket, orders: Order[][]): Order[][] => {
+  let maxPrice:number = getMaxPrice(price)
+
+  let filteredOrders: Order[][] = []
+
+  orders.forEach(restaurant => {
+    let filteredDish = restaurant.filter(dish => dish.price <= maxPrice)
+    filteredOrders.push(filteredDish)
+  })
+
+  return filteredOrders
+}
+
+const printOrders = (restaurants: Restuanrant[], filteredOrders: Order[][]) :string => {
+  let output = ""
+
+  restaurants.forEach((res, index) => {
+    if (filteredOrders[index].length > 0) {
+      output += res.name + "\n";
+      orders[index].forEach(dish => {
+        output += "- " + dish.name + ": $" + dish.price + "\n"
+      })
+    }
+  })
+
+  return output
+}
+
